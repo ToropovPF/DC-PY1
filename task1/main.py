3f90c8ab70e53c556b036eca588df74c95de12ff
ToropovPF <115175252+ToropovPF@users.noreply.github.com>
2022-10-12 00:02:10 +0300

    dasas

---
+++ task1/main.py
@@ -0,0 +1,6 @@
+list_ = [8, 9, -5, -3, 1, -10, 8, -10, -5, 0, 5, -4, 0, 10, -8, 1, 6, -6, 6, -9]
+total = sum(set(list_))
+count = len(set(list_))
+print(total)
+print(count)
+print(round(total / count, 5))
